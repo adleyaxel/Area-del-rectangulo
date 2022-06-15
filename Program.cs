698fc5bf4021279e1faf545702d1c3331c49fc6c
adleyaxel <adleymarchena@gmail.com>
2022-06-15 08:07:33 -0600

    Area-R

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Area de Rectangulo");
+Console.WriteLine("Introduzca base");
+
+//double
+double baser =
+Convert.ToDouble(Console.ReadLine());
+
+//Proceso
+Console.WriteLine("Introduzca altura");
+double altura =
+Convert.ToDouble(Console.ReadLine());
+
+double area = (baser* altura);
+
+Console.WriteLine("El area dle rectangulo es: " + area);
